c9062ee62460dd45e7795b84dfdf838fefb2c1bd
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-06-27 08:42:21 +0545

    Update nvim colorscheme

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+}
