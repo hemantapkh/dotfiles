0d8dbc4a003a9d0f040a2039e9a86c8961591334
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-08-21 18:05:52 +0545

    Add omz plugins and gcloud alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -5,3 +5,7 @@ alias dotfiles="lazygit -g ~/.local/share/yadm/repo.git --work-tree=$HOME"
 alias ds="dotfiles"
 alias lg="lazygit"
 alias ls="eza --icons=always"
+
+# Gcloud
+alias gcal="gcloud auth login"
+alias gcaal="gcloud auth application-default login"
